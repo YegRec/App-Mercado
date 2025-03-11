7a0d6649aa968b7c041449dc3bcc7c3f9461f907
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-03-10 20:37:35 -0400

    Creacion de proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMercado
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMercado
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
